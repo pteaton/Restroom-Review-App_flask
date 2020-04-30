efe20cc9757284ef4e2a747ed938a2c9876ceb0d
peaton373 <peaton373@gmail.com>
2020-04-30 11:05:41 -0500

    server running, hello world test, successful

--- app.py
+++ app.py
@@ -7,5 +7,12 @@ PORT=8000
 
 app = Flask(__name__)
 
+
+# print hellow world, test
+@app.route('/')
+def hello():
+	return 'Hello, world!'
+
 if __name__== '__main__':
 	app.run(debug=DEBUG, port=PORT)
+
