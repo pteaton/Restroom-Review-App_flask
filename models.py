222e1307f394cccefc08302cc0fc6b38232086a8
peaton373 <peaton373@gmail.com>
2020-04-30 15:30:48 -0500

    added UserMixin to model, updated model and reviews/users

--- models.py
+++ models.py
@@ -4,10 +4,10 @@ from flask_login import UserMixin
 
 DATABASE = SqliteDatabase('reviews.sqlite')
 
-class User(Model):
+class User(UserMixin, Model):
 	username=CharField(unique=True)
 	email=CharField(unique=True)
-	password=CharField(unique=True)
+	password=CharField()
 
 	class Meta:
 		database = DATABASE
